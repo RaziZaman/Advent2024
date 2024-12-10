b7ea255e6ce024db04d1928643b8806b5a1e4225
RaziZaman <145681484+RaziZaman@users.noreply.github.com>
2024-12-10 14:03:20 -0500

    10

--- Advent2024/src/Day52024Part2.java
+++ Advent2024/src/Day52024Part2.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Day52024 {
+public class Day52024Part2 {
     public static void main(String[] args) {
 
         ArrayList<String> fileData = getFileData("src/day5input.txt");
@@ -27,6 +27,8 @@ public class Day52024 {
     }
     public static int checkList(String list, ArrayList<String> rules) {
         boolean check = true;
+        int leftCount = 0;
+        int rightCount = 0;
         for (int i = 0; i < rules.size(); i++) {
             if (list.contains(rules.get(i).substring(0,2)) && list.contains(rules.get(i).substring(3,5))) {
                 if (list.indexOf(rules.get(i).substring(0,2)) > list.indexOf(rules.get(i).substring(3,5))) {
@@ -36,8 +38,26 @@ public class Day52024 {
         }
         if (check) {
             return Integer.parseInt(list.substring(list.length() / 2 - 1, list.length() / 2 + 1));
+        } else {
+            for (int i = 0; i < list.length() - 1; i += 3) {
+                for (int j = 0; j < rules.size(); j++) {
+                    if (list.contains(rules.get(i).substring(0,2)) && list.contains(rules.get(i).substring(3,5)) && rules.get(i).contains(list.substring(i, i + 2))) {
+                        if (rules.get(i).substring(0,2).contains(list.substring(i, i + 2))) {
+                            leftCount++;
+                        }
+                        if (rules.get(i).substring(3,5).contains(list.substring(i, i + 2))) {
+                            rightCount++;
+                        }
+                    }
+                }
+                if (leftCount == rightCount) {
+                    return Integer.parseInt(list.substring(i,i + 2));
+                }
+                leftCount = 0;
+                rightCount = 0;
+            }
         }
-        return 0;
+        return 999999999;
     }
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
